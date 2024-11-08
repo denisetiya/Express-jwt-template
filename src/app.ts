d1c533ac5fecf69661498a160d7503eb4e8ad42c
deni setiya <denisetiya009@gmail.com>
2024-11-08 10:44:04 +0700

    ✨ feat:add rate limiter

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,22 @@ import express, { Request, Response } from "express";
 import url from "./url";
 import { authenticateJWT } from "./middleware/jwt.middleware";
 import response from "./utils/response.api";
-
+import rateLimit from "express-rate-limit";
 
 const app = express();
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, 
+    max: 10, 
+    message: 'Too many requests from this IP, please try again later.',
+});
+
+app.use(limiter);
+
 app.use(express.json());
 
 app.use("/v1/",authenticateJWT, url);
+
+
 
 app.use((req: Request, res: Response) => {
     response(res, 404, "Not Found", "are you developer or hacker ?");
